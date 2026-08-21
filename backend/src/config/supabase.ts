4a10b63fd30ba1503469704a25aa124b3c6e3b50
jisig <irfanshaikh10028@gmail.com>
2026-08-21 19:22:16 +0530

    Resolve merge conflicts

--- backend/src/config/supabase.ts
+++ backend/src/config/supabase.ts
@@ -7,22 +7,5 @@ dotenv.config();
 
 export const supabase = createClient(
   process.env.SUPABASE_URL!,
-<<<<<<< HEAD
-  process.env.SUPABASE_ANON_KEY!,
-);
-
-// import * as dotenv from 'dotenv';
-// import { createClient } from '@supabase/supabase-js';
-
-// dotenv.config();
-
-// console.log(process.env.SUPABASE_URL);
-
-// export const supabase = createClient(
-//   process.env.SUPABASE_URL!,
-//   process.env.SUPABASE_ANON_KEY!,
-// );
-=======
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
 );
->>>>>>> origin/raza
